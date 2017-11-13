a7108093f6452958e6b901ba5e2892c6d085da1b
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-13 14:41:08 -0500

    Change text when the lights are turned on and off

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 var body = document.querySelector('body');
 var button = document.querySelector('button');
-var status = document.querySelector('.status');
+var sta = document.querySelector('.status');
 
 button.addEventListener('click', pressSwitch);
 
 function pressSwitch() {
-  console.log(button.classList.contains('on'));
   if (button.classList.contains('on')) {
     button.classList.remove('on');
     button.classList.add('off');
     body.className = 'dark';
+    sta.innerText = "Hey, who turned off the lights?";
   } else {
     button.classList.remove('off')
     button.classList.add('on')
     body.className = 'light';
+    sta.innerText = "It's so bright in here!";
   }
 }
