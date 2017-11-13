b04b89884997d185a32bb517427348e48d5fe7f9
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-13 14:26:36 -0500

    Add variable for status, and remove pressSwitch function from variable

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-var body = document.querySelector('body')
-var button = document.querySelector('button')
+var body = document.querySelector('body');
+var button = document.querySelector('button');
+var status = document.querySelector('.status');
 
-button.addEventListener('click', pressSwitch)
+button.addEventListener('click', pressSwitch);
 
-var pressSwitch = function() {
-  if (button.classList.contains('on')){
+function pressSwitch() {
+  console.log(button.classList.contains('on'));
+  if (button.classList.contains('on')) {
     button.classList.remove('on');
     button.classList.add('off');
     body.className = 'dark';
   } else {
     button.classList.remove('off')
-    button.className.add('on')
+    button.classList.add('on')
     body.className = 'light';
   }
 }
