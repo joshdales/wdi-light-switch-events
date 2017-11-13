fdd8023ecd16855452018eeb3d2581b58a83d842
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-13 14:08:34 -0500

    Define function to change between light and dark class themes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
+var body = document.querySelector('body')
+var button = document.querySelector('button')
 
 
+var pressSwitch = function() {
+  if (button.classList.contains('on')){
+    body.className = 'dark';
+  } else {
+    body.className = 'light';
+  }
+}
