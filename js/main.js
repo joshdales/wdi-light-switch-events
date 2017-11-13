421af02a193e094b02334e99c8acb97034ae6930
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-11-13 14:21:07 -0500

    Add and remove button class to the pressSwitch function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 var body = document.querySelector('body')
 var button = document.querySelector('button')
 
+button.addEventListener('click', pressSwitch)
 
 var pressSwitch = function() {
   if (button.classList.contains('on')){
+    button.classList.remove('on');
+    button.classList.add('off');
     body.className = 'dark';
   } else {
+    button.classList.remove('off')
+    button.className.add('on')
     body.className = 'light';
   }
 }
